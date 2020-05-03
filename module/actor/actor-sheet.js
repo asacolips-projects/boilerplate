998bd8f2d23a742119da057453f78bc21dfd1d8f
Matt Smith <matt.smith@mediacurrent.com>
2020-05-03 11:57:49 -0500

    Add rollables

--- module/actor/actor-sheet.js
+++ module/actor/actor-sheet.js
@@ -50,6 +50,9 @@ export class BoilerplateActorSheet extends ActorSheet {
       this.actor.deleteOwnedItem(li.data("itemId"));
       li.slideUp(200, () => this.render(false));
     });
+
+    // Rollable abilities.
+    html.find('.rollable').click(this._onRoll.bind(this));
   }
 
   /* -------------------------------------------- */
@@ -79,6 +82,26 @@ export class BoilerplateActorSheet extends ActorSheet {
 
     // Finally, create the item!
     return this.actor.createOwnedItem(itemData);
+  }
+
+  /**
+   * Handle clickable rolls.
+   * @param {Event} event   The originating click event
+   * @private
+   */
+  _onRoll(event) {
+    event.preventDefault();
+    const element = event.currentTarget;
+    const dataset = element.dataset;
+
+    if (dataset.roll) {
+      let roll = new Roll(dataset.roll, this.actor.data.data);
+      let label = dataset.label ? `Rolling ${dataset.label}` : '';
+      roll.roll().toMessage({
+        speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+        flavor: label
+      });
+    }
   }
 
 }
